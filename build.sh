f825959c7a260c73d77651f26c6faa75b9388f5b
Naveen Kumaran Appathurai <Naveen Kumaran Appathurai>
2020-05-04 22:50:55 +0530

    magic with ibmcloud dev enable

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 sudo docker build -t naveenkumarana/spring-demo -f docker/Dockerfile .
 sudo docker push naveenkumarana/spring-demo
 sudo docker run --detach -p 9090:8080 naveenkumarana/spring-demo
+
+## K8 stuff
+## Get Node port IP
+nodeip=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}' | awk '{print $1}')
+port=$(kubectl --namespace springdemo get service springdemoservice -o jsonpath='{.spec.ports[0].nodePort}')
+echo "http://${nodeip}:${port}"
+
